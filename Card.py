d26f760cd1fc5acfcaa4820b778e4347cce8f385
gabbyfs-dot <gabbyfsschool@gmail.com>
2026-01-15 00:33:25 -0500

    updatecard.py

--- Card.py
+++ Card.py
@@ -1,31 +1,10 @@
 class Card:
-    """
-    Represents a single playing card (rank + suit).
-    Owns a dictionary mapping ranks -> point values (as required).
-    """
 
-# dictionary of ranks and point values (REQUIRED)
-rank_points = {
-    "Two": 2,
-    "Three": 3,
-    "Four": 4,
-    "Five": 5,
-    "Six": 6,
-    "Seven": 7,
-    "Eight": 8,
-    "Nine": 9,
-    "Ten": 10,
-    "Jack": 10,
-    "Queen": 10,
-    "King": 10,
-    "Ace": 11
-    }
 
-def __init__(self, rank="Ace", suit="Spades"):
+ def __init__(self, rank="Ace", suit="Spades"):
     self.rank = rank
     self.suit = suit
 
-    # Big 3
 def __str__(self):
     return self.rank + " of " + self.suit
 
